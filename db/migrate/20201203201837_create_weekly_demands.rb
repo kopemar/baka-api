a57fb34475eb0d97252507f9df67a82995dc529f
Martina Kopecka <xykopecka@gmail.com>
2020-12-03 21:27:08 +0100

    Update weekly demand migration

--- db/migrate/20201203201837_create_weekly_demands.rb
+++ db/migrate/20201203201837_create_weekly_demands.rb
@@ -1,12 +1,14 @@
 class CreateWeeklyDemands < ActiveRecord::Migration[6.0]
   def change
     create_table :weekly_demands do |t|
-      t.integer :week
-      t.integer :year
+      t.integer :week, null: false, default: 0
+      t.integer :year, null: false, default: 2020
 
-      t.string :demand
+      t.string :demand, null: false
 
       t.timestamps
     end
+
+    add_index :weekly_demands, [:week, :year], unique: true
   end
 end
