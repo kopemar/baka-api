1db63328918f5ad6ad3ce94311e427762a86b140
Martina Kopecka <xykopecka@gmail.com>
2021-01-01 10:57:34 +0100

    Add API status codes

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -23,46 +23,70 @@ class ShiftController < ApplicationController
   end
 
   def assign_shift
+    if params[:schedule_id].nil?
+      return render :status => :bad_request, :json => {:errors => ["schedule_id not defined"]}
+    end
     contract = Contract::active_agreements.where(employee_id: current_user.id).where(schedule_id: params[:schedule_id]).first
-    unless contract.nil?
-      shift = Shift.where(id: params[:id]).first
+    shift = Shift.where(id: params[:id]).first
+    errors = Array.new
+    if shift.nil?
+      render :status => :not_found, json: {:errors => {
+          :message => "Shift not found!"
+      }}
+    elsif !contract.nil?
       if shift.schedule_id.nil?
         shift.schedule_id = contract.schedule_id
         shift.user_scheduled = true
         if shift.save!
           return render json: shift
+        else
+          errors.push("Could not save shift")
         end
+      else
+        errors.push("Schedule ID is not nil")
       end
     end
-    render :status => :bad_request
+    render :status => :unprocessable_entity, :json => {:errors => errors}
   end
 
   def get_unassigned_shifts
     overlaps = Shift.where(schedule_id: Schedule.where(contract_id: Contract.where(employee_id: current_user.id).all.map { |d| d.id })).map { |d| "end_time >= '#{d.start_time}' AND start_time <= '#{d.end_time}'" }.join(" OR ")
-    shift = if params[:start_date].nil? && params[:end_date].nil?
-              Shift::unassigned.where.not(overlaps).order('shifts.start_time')
-            elsif params[:start_date].nil?
-              Shift::unassigned::planned_before(params[:end_date].to_datetime).where.not(overlaps).order('shifts.start_time DESC')
-            elsif params[:end_date].nil?
-              Shift::unassigned::planned_after(params[:start_date].to_datetime).where.not(overlaps).order('shifts.start_time')
-            else
-              Shift::unassigned::planned_between(params[:start_date].to_datetime, params[:end_date].to_datetime).where.not(overlaps).order('shifts.start_time')
-            end
-    return shift
+    if params[:start_date].nil? && params[:end_date].nil?
+      Shift::unassigned.where.not(overlaps).order('shifts.start_time')
+    elsif params[:start_date].nil?
+      Shift::unassigned::planned_before(params[:end_date].to_datetime).where.not(overlaps).order('shifts.start_time DESC')
+    elsif params[:end_date].nil?
+      Shift::unassigned::planned_after(params[:start_date].to_datetime).where.not(overlaps).order('shifts.start_time')
+    else
+      Shift::unassigned::planned_between(params[:start_date].to_datetime, params[:end_date].to_datetime).where.not(overlaps).order('shifts.start_time')
+    end
   end
 
   def remove_from_schedule
     shift = Shift.where(id: params[:id]).first
+    errors = Array.new
     if shift.user_scheduled && ((shift.start_time - DateTime::now).to_i / 1.day) > 4
       shift.schedule_id = nil
       shift.save!
       return render json: shift
+    else
+      unless shift.user_scheduled
+        errors.push("Not scheduled by this user!")
+      end
+      unless ((shift.start_time - DateTime::now).to_i / 1.day) > 4
+        errors.push("Starts in less than 4 days!")
+      end
     end
-    render :status => :unauthorized
+    render :status => :unprocessable_entity, :json => {:errors => errors}
   end
 
   def get_possible_schedules
-    schedules = Schedule.where(id: Contract::active_agreements::where(employee_id: current_user.id).map { |c| c.schedule_id })
-    render json: {:schedules => schedules}
+    shift = Shift.where(id: params[:id]).first
+    if shift.nil?
+      render :status => :not_found, json: {:errors => ["Shift not found!"]}
+    else
+      schedules = Schedule.where(id: Contract::active_agreements::where(employee_id: current_user.id).map { |c| c.schedule_id })
+      render json: {:schedules => schedules}
+    end
   end
 end
