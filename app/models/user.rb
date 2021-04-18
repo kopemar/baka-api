6e4ca235c6d6be45a57df8b43169df4cbc04a8ea
Martina Kopecka <xykopecka@gmail.com>
2021-04-18 07:44:58 +0200

    Fix RegEx for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable
   include DeviseTokenAuth::Concerns::User
   has_many :fcm_tokens
+  validate :validate_username
 
   belongs_to :organization
 
@@ -24,14 +25,20 @@ class User < ApplicationRecord
     ROLE.key?(type.downcase.to_sym) ? ROLE[type.downcase.to_sym] : super
   end
 
-  def is_manager?
+  def manager?
     self.type == "Manager"
   end
 
   def as_json(*args)
     hash = super(*args)
-    hash.merge({manager: is_manager?, organization_name: self.organization.name, organization_id: self.organization_id})
+    hash.merge({manager: manager?, organization_name: self.organization.name, organization_id: self.organization_id})
   end
 
+  # todo regex
+  def validate_username
+    unless self.username.match?(/[A-z0-9]+([A-z0-9.]).([A-z0-9]+)+/)
+      errors.add(:username, "username does not match pattern")
+    end
+  end
 
 end
