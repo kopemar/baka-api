51e12f61c24d2ff165741413bdf4897687d2d274
Martina Kopecka <xykopecka@gmail.com>
2021-05-05 08:54:40 +0200

    Comment out FCM notifications for production

--- app/helpers/notification_helpers.rb
+++ app/helpers/notification_helpers.rb
@@ -11,15 +11,15 @@ class NotificationHelpers
   # }
   def self.send_notification(users, options)
 
-    server_key = Rails.application.credentials.firebase[:key]
-    fcm_client = FCM.new(server_key)
-
-    tokens = FcmToken.select(:fcm_token).where(user_id: users.map(&:id)).map(&:fcm_token).to_a
-    Rails.logger.debug "🐼 Gonna try to send notification to users #{users.map(&:username)} "
-
-    tokens.each_slice(20) do |ids|
-      fcm_client.send(ids, options)
-      Rails.logger.debug "🐼 Sent notification to users #{ids} "
-    end
+    # server_key = Rails.application.credentials.firebase[:key]
+    # fcm_client = FCM.new(server_key)
+    #
+    # tokens = FcmToken.select(:fcm_token).where(user_id: users.map(&:id)).map(&:fcm_token).to_a
+    # Rails.logger.debug "🐼 Gonna try to send notification to users #{users.map(&:username)} "
+    #
+    # tokens.each_slice(20) do |ids|
+    #   fcm_client.send(ids, options)
+    #   Rails.logger.debug "🐼 Sent notification to users #{ids} "
+    # end
   end
 end
